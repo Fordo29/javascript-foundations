89dfafca1ef1f401bb3c5c45dcf57705a2bf2d7c
Christine Rowland <fordo29@gmail.com>
2021-10-18 23:39:45 -0600

    Initial commit

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,12 @@
+class Vampire {
+  constructor(name, pet) {
+    this.name = name;
+    if (pet === undefined) {
+      this.pet = "bat";
+    } else {
+      this.pet = pet;
+    }
+  }
+}
+
+module.exports = Vampire;
