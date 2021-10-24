c93122862f32ed404e97adc8d6941a06ee8cc81f
Christine Rowland <fordo29@gmail.com>
2021-10-24 10:13:24 -0600

    Submit pirate exercise

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -2,12 +2,32 @@ class Pirate {
   constructor(name, job) {
     this.name = name;
     this.cursed = false;
+    this.booty = 0;
     if (job === undefined) {
       this.job = "scallywag";
     } else {
       this.job = job;
+    }
   }
-}
+  robShip() {
+    this.booty += 100;
+    if (this.booty <= 500) {
+      return 'YAARRR!'
+    } else {
+      this.booty -= 100;
+      this.cursed = true;
+      return 'ARG! I\'ve been cursed!'
+    }
+  }
+  liftCurse() {
+    if (this.cursed === false) {
+      return 'You don\'t need to lift a curse!'
+    }else if (this.cursed === true) {
+        this.booty -= 300;
+        this.cursed = false;
+        return 'Your curse has been lifted!'
+      }
+  }
 }
 
 
