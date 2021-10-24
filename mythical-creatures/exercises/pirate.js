5684332ca616f6a65110f07370e7bdfd168631d9
Christine Rowland <fordo29@gmail.com>
2021-10-23 23:09:36 -0600

    Submit pirate in progress

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -1,4 +1,14 @@
-
+class Pirate {
+  constructor(name, job) {
+    this.name = name;
+    this.cursed = false;
+    if (job === undefined) {
+      this.job = "scallywag";
+    } else {
+      this.job = job;
+  }
+}
+}
 
 
 module.exports = Pirate;
