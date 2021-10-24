0b0b75fa0e7234dbe325182b13f91e4c32b7b51e
Christine Rowland <fordo29@gmail.com>
2021-10-23 22:13:00 -0600

    Submit dragon and vampire

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,14 +1,23 @@
 class Vampire {
   constructor(name, pet) {
     this.name = name;
-
+    this.ouncesDrank = 0;
+    this.thirsty = true;
     if (pet === undefined) {
       this.pet = "bat";
     } else {
       this.pet = pet;
     }
-    this.thirsty = true;
   }
+
+    drink() {
+      this.thirsty = false;
+      if (this.ouncesDrank < 50) {
+        this.ouncesDrank += 10;
+      } else {
+        return `I\'m too full to drink anymore!`;
+      }
+    }
 }
 
 module.exports = Vampire;
