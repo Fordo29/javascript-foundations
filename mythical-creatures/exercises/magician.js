0486b3aaadf0c40b49c43d1496030f06d1e55c1f
Christine Rowland <fordo29@gmail.com>
2021-10-25 18:38:11 -0600

    Submit magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,13 +1,32 @@
 class Magician {
-  constructor(name) {
-    this.name = `The Great ${name.name}`;
-    this.assistant = name.assistant;
-    if(favoriteAccessory === undefined) {
+  constructor({name:name, assistant: assistant, clothing:clothing}) {
+    this.name = `The Great ${name}`;
+    this.assistant = assistant;
+    this.confidencePercentage = 10;
+    if (clothing ===  undefined) {
       this.favoriteAccessory = "top hat";
     } else {
-      this.favoriteAccessory = favoriteAccessory;
+      this.favoriteAccessory = clothing;
+    }
+  }
+  performIncantation(magicTrick) {
+    return `${magicTrick.toUpperCase()}!`;
+  }
+  performTrick() {
+    this.confidencePercentage += 10;
+    if(this.favoriteAccessory === "top hat"){
+      return 'PULL RABBIT FROM TOP HAT';
+    } else {
+      return 'PULL DOVE FROM SLEEVE';
     }
 
+    }
+    performShowStopper() {
+      if(this.confidencePercentage < 100 || this.assistant === false) {
+        return 'Oh no, this trick is not ready!';
+      } else {
+        return 'WOW! The magician totally just sawed that person in half!';
+      }
   }
 }
 
