8bc325202df434225b44ef8a8b32201e16c9794b
Christine Rowland <fordo29@gmail.com>
2021-11-09 21:05:08 -0700

    Submit Trick or Treat exercises

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,26 @@
+var Snack = require('./snack');
 class LunchBox {
+  constructor(lunch) {
+    this.owner = lunch.owner;
+    this.material = lunch.madeOf;
+    this.shape = lunch.shape;
+    this.color = lunch.color;
+    this.snacks = [];
+    this.healthySnacks = [];
+  }
 
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+    if (snack.type.includes("fruit") || snack.type.includes("Fruit")) {
+      this.healthySnacks.push(snack.type);
+
+    }
+  }
+
+  findHealthySnacks() {
+    return this.healthySnacks
+  }
 }
 
 module.exports = LunchBox;
--- skate-park/skatepark.js
+++ skate-park/skatepark.js
@@ -9,17 +9,18 @@ class SkatePark {
     this.occupants = [];
   }
   admit(skater) {
-    if(this.cost === 0 && this.occupants < 4) {
+    if(this.cost === 0) {
       this.occupants.push(skater);
       return 'Welcome to the free Major Taylor Skatepark!'
-    } else if (skater.money > this.cost && this.occupants < 4) {
-      skater.money = skater.money -= this.cost
+    } else if (skater.money >= this.cost && this.occupants.length < 3) {
+      skater.money = skater.money - this.cost;
       this.occupants.push(skater);
       return `Welcome to Curbside, the cost will be $${this.cost}.00.`
-    } else if (this.cost > skater.money && this.occupants < 4) {
+    } else if (this.cost > skater.money) {
       return `Sorry, you don't have enough money.`
-    }
+    } else {
       return 'Sorry, we are at max capacity. Thank you for understanding.'
+    }
   }
 }
 
